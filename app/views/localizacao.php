3c1c40b313e99a90bba0c5226daa64c5ebad4cd1
Emerson Martins Brito <emersonmrbr@gmail.com>
2022-06-26 00:52:22 -0300

    Responsivo localização concluído

--- app/views/localizacao.php
+++ app/views/localizacao.php
@@ -1,5 +1,5 @@
+<div class="c-location">
 <h1>Localização</h1>
-<p></p>
 <div class="c-location__maps js-maps"></div>
 <article class="c-location__address">
     <h3>Núcleo MAP Máquinas Automação e Programação</h3>
@@ -8,3 +8,4 @@
     <p></p>
     <p>Fone: +55 992 749 270 </p>
 </article>
+</div>
