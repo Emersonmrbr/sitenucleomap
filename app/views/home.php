7c181d1f338eba3548da392a9c56a8f00a9d06f3
Emerson <emersonmrbr@gmail.com>
2023-01-02 18:54:06 -0300

    Inserido a tag picture na home

--- app/views/home.php
+++ app/views/home.php
@@ -1,42 +1,54 @@
 <div class="c-slider">
     <div class="c-slider__gallery-wrapper">
         <figure class="c-slider__gallery js-carousel">
-            <img src="app/assets/image/automacaoindustrial/image4.jpg" alt="Foto 4 máquina Núcleo MAP" class="c-slider__item">
+            <picture class="c-slider__item">
+                <img srcset="app/assets/image/automacaoindustrial/image4.jpg" alt="Foto 4 máquina Núcleo MAP">
+            </picture>
             <figcaption class="c-slider__description">
                 <h3 class="c-slider__title">Consultoria</h3>
                 <p class="c-slider__text">Damos palpites que geram economias e produtividade.</p>
             </figcaption>
         </figure>
         <figure class="c-slider__gallery">
-            <img src="app/assets/image/automacaoindustrial/image1.jpg" alt="Foto 1 máquina Núcleo MAP" class="c-slider__item">
+            <picture class="c-slider__item">
+                <img srcset="app/assets/image/automacaoindustrial/image1.jpg" alt="Foto 1 máquina Núcleo MAP">
+            </picture>
             <figcaption class="c-slider__description">
                 <h3 class="c-slider__title">Qualidade, tecnologia e inovação</h3>
                 <p class="c-slider__text">Buscando sempre inovar com o que há melhor no mercado.</p>
             </figcaption>
         </figure>
         <figure class="c-slider__gallery">
-            <img src="app/assets/image/automacaoindustrial/image2.jpg" alt="Foto 2 máquina Núcleo MAP" class="c-slider__item">
+            <picture class="c-slider__item">
+                <img srcset="app/assets/image/automacaoindustrial/image2.jpg" alt="Foto 2 máquina Núcleo MAP">
+            </picture>
             <figcaption class="c-slider__description">
                 <h3 class="c-slider__title">Experiencia e dedicação</h3>
                 <p class="c-slider__text">Ampla experiencia e expertise de aprendizagem contínua.</p>
             </figcaption>
         </figure>
         <figure class="c-slider__gallery">
-            <img src="app/assets/image/automacaoindustrial/image3.jpg" alt="Foto 3 máquina Núcleo MAP" class="c-slider__item">
+            <picture class="c-slider__item">
+                <img srcset="app/assets/image/automacaoindustrial/image3.jpg" alt="Foto 3 máquina Núcleo MAP">
+            </picture>
             <figcaption class="c-slider__description">
                 <h3 class="c-slider__title">IOT e industria 4.0</h3>
                 <p class="c-slider__text">Preparados para soluções de ponta a ponta.</p>
             </figcaption>
         </figure>
         <figure class="c-slider__gallery">
-            <img src="app/assets/image/automacaoindustrial/image4.jpg" alt="Foto 4 máquina Núcleo MAP" class="c-slider__item">
+            <picture class="c-slider__item">
+                <img srcset="app/assets/image/automacaoindustrial/image4.jpg" alt="Foto 4 máquina Núcleo MAP">
+            </picture>
             <figcaption class="c-slider__description">
                 <h3 class="c-slider__title">Consultoria</h3>
                 <p class="c-slider__text">Damos palpites que geram economias e produtividade.</p>
             </figcaption>
         </figure>
         <figure class="c-slider__gallery">
-            <img src="app/assets/image/automacaoindustrial/image1.jpg" alt="Foto 1 máquina Núcleo MAP" class="c-slider__item">
+            <picture class="c-slider__item">
+                <img srcset="app/assets/image/automacaoindustrial/image1.jpg" alt="Foto 1 máquina Núcleo MAP">
+            </picture>
             <figcaption class="c-slider__description">
                 <h3 class="c-slider__title">Qualidade, tecnologia e inovação</h3>
                 <p class="c-slider__text">Buscando sempre inovar com o que há melhor no mercado.</p>
