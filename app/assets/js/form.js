543142236de9a34bf329478ac67ceddee0c27afc
Emersonmrbr <emersonmrbr@gmail.com>
2023-01-27 00:18:36 -0300

    Form solved clear

--- app/assets/js/form.js
+++ app/assets/js/form.js
@@ -1,13 +1,3 @@
-// contactInput.forEach((input) => {
-//   input.addEventListener("blur", function(){
-//   contactLabel.forEach((label) => {
-//     label.classList.remove("is-filled");
-//     if (label.htmlFor === input.id) {
-//       element.classList.add("is-filled");
-//     }
-//     console.log(element);
-//   });
-// });
 contactInput.forEach((input, index) => {
   input.addEventListener("blur", function () {
     contactLabel.forEach((label) => {
